7dfad3e790c0e9a437dce2e2dfe40dd440b96f30
Kim Minseok <semteul32@gmail.com>
2025-04-30 16:30:25 +0900

    feat(test db): DB test db에 삭제 준비
    /cancelDB DB 내용중 주입된 테스트 데이터만 지움
    /resetDB DB 테이블 안의 모든 레코드를 삭제
    
    close #41

--- src/main/java/moanote/backend/controller/TestController.java
+++ src/main/java/moanote/backend/controller/TestController.java
@@ -2,14 +2,12 @@ package moanote.backend.controller;
 
 import moanote.backend.entity.Note;
 import moanote.backend.entity.UserData;
-import moanote.backend.repository.NoteRepository;
-import moanote.backend.repository.UserDataRepository;
+import moanote.backend.repository.NoteUserDataRepository;
 import moanote.backend.service.NoteService;
 import moanote.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
-import java.util.Map;
+import java.util.ArrayList;
 
 /**
  * DB를 초기화합니다.
@@ -18,29 +16,80 @@ import java.util.Map;
 @RestController
 @RequestMapping("/") // 엔드포인트 경로 정의
 public class TestController {
+
   @Autowired
   NoteService noteService;
 
   @Autowired
   UserService userService;
 
+  @Autowired
+  private NoteUserDataRepository noteUserDataRepository;
+
+  private ArrayList<UserData> users = new ArrayList<>();
+  private ArrayList<Note> notes = new ArrayList<>();
+
+  /**
+   * DB에 테스트 데이터 주입하는 기능
+   *
+   * @return
+   */
   @GetMapping("/initDB")
   public String initDB() {
     try {
-      UserData u1 = userService.createUser("kim","moanote1234");
-      UserData u2 = userService.createUser("sa","moanote1234");
-      UserData u3 = userService.createUser("son","moanote1234");
+      users.add(userService.createUser("kim", "moanote1234"));
+      users.add(userService.createUser("sa", "moanote1234"));
+      users.add(userService.createUser("son", "moanote1234"));
+      users.add(userService.createUser("moa-bot-id", "moanote1234"));
 
-      UserData bot = userService.createUser("moa-bot-id","moanote1234");
+      notes.add(noteService.createNote(users.get(0).getId()));
+      notes.add(noteService.createNote(users.get(1).getId()));
+      notes.add(noteService.createNote(users.get(2).getId()));
 
-      Note n1 = noteService.createNote(u1.getId());
-      Note n2 = noteService.createNote(u2.getId());
-      Note n3 = noteService.createNote(u3.getId());
+      noteService.updateNote(notes.get(0).getId(), "노트1");
+      noteService.updateNote(notes.get(1).getId(), "노트2");
+      noteService.updateNote(notes.get(2).getId(), "노트3");
 
-      noteService.updateNote(n1.getId(),"노트1의 내용입니다.");
-      noteService.updateNote(n1.getId(),"노트2의 내용입니다.");
-      noteService.updateNote(n1.getId(),"노트3의 내용입니다.");
+      return "<html><body><h1>테스트 데이터 주입 완료</h1></body></html>";
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+      return "<html><body><h1>오류 발생</h1></body></html>";
+    }
+  }
 
+  /**
+   * DB에 주입한 테스트 데이터 삭제하는 기능
+   *
+   * @return 결과 HTML
+   */
+  @GetMapping("/cancelDB")
+  public String cancelDB() {
+    try {
+      for (Note note : notes) {
+        noteService.delete(note);
+      }
+      for (UserData user : users) {
+        userService.delete(user);
+      }
+      return "<html><body><h1>테스트 데이터 삭제 완료</h1></body></html>";
+    } catch (Exception e) {
+      return "<html><body><h1>오류 발생</h1></body></html>";
+    }
+  }
+
+  /**
+   * DB에 저장된 모든 레코드를 지우는 기능
+   *
+   * @return 결과 HTML
+   */
+  @GetMapping("/resetDB")
+  public String resetDB() {
+    try {
+      // TODO textChatMessageRepository 구현시 delete 실시
+      // textChatMessageRepository.deleteAll();
+      noteUserDataRepository.deleteAll();
+      noteService.deleteAll();
+      userService.deleteAll();
       return "<html><body><h1>DB 초기화 완료</h1></body></html>";
     } catch (Exception e) {
       return "<html><body><h1>오류 발생</h1></body></html>";
