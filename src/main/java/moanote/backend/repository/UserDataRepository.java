fb1966d309e11ad5b1a2eaf1d25700e55fcf400d
SONY-STRING <sctoa1@gmail.com>
2025-06-05 18:02:32 +0900

    refactor(userCreate): 유저 생성 시 DB key 레코드 생성 로직을 Repository 레이어에 이동

--- src/main/java/moanote/backend/repository/UserDataRepository.java
+++ src/main/java/moanote/backend/repository/UserDataRepository.java
@@ -1,9 +1,17 @@
 package moanote.backend.repository;
 
+import com.github.f4b6a3.uuid.UuidCreator;
 import moanote.backend.entity.UserData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface UserDataRepository extends JpaRepository<UserData, UUID> {
-    UserData findByUsername(String username);
+  default UserData create(String username, String password) {
+    UserData userData = new UserData();
+    userData.setUsername(username);
+    userData.setPassword(password);
+    userData.setId(UuidCreator.getTimeOrderedEpoch());
+    return save(userData);
+  }
+  UserData findByUsername(String username);
 }
