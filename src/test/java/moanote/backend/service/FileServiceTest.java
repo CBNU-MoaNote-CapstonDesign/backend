421eb158803d9ac15ff97354e3b2671f55bfc803
SONY-STRING <sctoa1@gmail.com>
2025-10-12 09:18:06 +0900

    test(deleteFile): 현재 존재하는 제약조건 위반 버그를 재현하는 deleteNestedDirectory 유닛 테스트 추가

--- src/test/java/moanote/backend/service/FileServiceTest.java
+++ src/test/java/moanote/backend/service/FileServiceTest.java
@@ -10,6 +10,10 @@ import moanote.backend.entity.File.FileType;
 import moanote.backend.entity.Note;
 import moanote.backend.entity.UserData;
 import moanote.backend.repository.FileRepository;
+import moanote.backend.repository.TextNoteSegmentRepository;
+import moanote.backend.repository.DiagramNoteSegmentRepository;
+import org.hibernate.exception.ConstraintViolationException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,6 +46,8 @@ class FileServiceTest {
   @Autowired
   private EntityManager entityManager;
 
+  @Autowired
+  private NoteService noteService;
 
   @Test
   void removeFilesRecursively() {
@@ -93,5 +99,63 @@ class FileServiceTest {
     Assert.isTrue(note != null, "Note should not be null");
     Assert.isTrue(file.getNote().getType().equals(Note.NoteType.CODE), "File should be of type CODE");
     Assert.isTrue(file.getNote().getCodeLanguage().equals(Note.CodeLanguage.JAVASCRIPT), "File should be of language JAVASCRIPT");
+  }
+
+  @Test
+  @Transactional
+  void deleteNestedDirectory() {
+    UserData user = userService.createUser("delete-test-user", "delete-test-password");
+    File rootDirectory = fileRepository.getRootDirectory(user);
+
+    File projectDirectory = fileService.createFile(user.getId(), "project", FileType.DIRECTORY, rootDirectory.getId());
+    File apiDirectory = fileService.createFile(user.getId(), "api", FileType.DIRECTORY, projectDirectory.getId());
+    File publicDirectory = fileService.createFile(user.getId(), "public", FileType.DIRECTORY, projectDirectory.getId());
+    File handlersDirectory = fileService.createFile(user.getId(), "handlers", FileType.DIRECTORY, apiDirectory.getId());
+    File utilsDirectory = fileService.createFile(user.getId(), "utils", FileType.DIRECTORY, handlersDirectory.getId());
+    File componentsDirectory = fileService.createFile(user.getId(), "components", FileType.DIRECTORY, publicDirectory.getId());
+    File layoutDirectory = fileService.createFile(user.getId(), "layout", FileType.DIRECTORY, componentsDirectory.getId());
+
+    File packageJson = fileService.createFile(user.getId(), "package.json", FileType.DOCUMENT, projectDirectory.getId());
+    File packageLock = fileService.createFile(user.getId(), "package-lock.json", FileType.DOCUMENT, projectDirectory.getId());
+    File serverJs = fileService.createFile(user.getId(), "server.js", FileType.DOCUMENT, projectDirectory.getId());
+
+    File problemJs = fileService.createFile(user.getId(), "problem.js", FileType.DOCUMENT, apiDirectory.getId());
+    File randomProblemsJs = fileService.createFile(user.getId(), "random-problems.js", FileType.DOCUMENT, apiDirectory.getId());
+    File routeHandlerJs = fileService.createFile(user.getId(), "route-handler.js", FileType.DOCUMENT, handlersDirectory.getId());
+    File authMiddlewareJs = fileService.createFile(user.getId(), "auth-middleware.js", FileType.DOCUMENT, utilsDirectory.getId());
+
+    File indexHtml = fileService.createFile(user.getId(), "index.html", FileType.DOCUMENT, publicDirectory.getId());
+    File scriptJs = fileService.createFile(user.getId(), "script.js", FileType.DOCUMENT, publicDirectory.getId());
+    File styleCss = fileService.createFile(user.getId(), "style.css", FileType.DOCUMENT, publicDirectory.getId());
+    File navbarJs = fileService.createFile(user.getId(), "navbar.js", FileType.DOCUMENT, componentsDirectory.getId());
+    File footerJs = fileService.createFile(user.getId(), "footer.js", FileType.DOCUMENT, componentsDirectory.getId());
+    File mainLayoutJs = fileService.createFile(user.getId(), "main-layout.js", FileType.DOCUMENT, layoutDirectory.getId());
+
+    noteService.createTextNoteSegment(packageJson.getNote().getId());
+    noteService.createTextNoteSegment(packageLock.getNote().getId());
+    noteService.createTextNoteSegment(serverJs.getNote().getId());
+    noteService.createTextNoteSegment(serverJs.getNote().getId());
+
+    noteService.createTextNoteSegment(problemJs.getNote().getId());
+    noteService.createTextNoteSegment(randomProblemsJs.getNote().getId());
+    noteService.createTextNoteSegment(routeHandlerJs.getNote().getId());
+    noteService.createTextNoteSegment(authMiddlewareJs.getNote().getId());
+    noteService.createTextNoteSegment(authMiddlewareJs.getNote().getId());
+
+    noteService.createTextNoteSegment(indexHtml.getNote().getId());
+    noteService.createTextNoteSegment(scriptJs.getNote().getId());
+    noteService.createTextNoteSegment(styleCss.getNote().getId());
+    noteService.createTextNoteSegment(navbarJs.getNote().getId());
+    noteService.createTextNoteSegment(footerJs.getNote().getId());
+    noteService.createTextNoteSegment(mainLayoutJs.getNote().getId());
+    noteService.createTextNoteSegment(mainLayoutJs.getNote().getId());
+
+    entityManager.flush();
+    entityManager.clear();
+
+    Assertions.assertDoesNotThrow(() -> {
+      fileService.deleteFile(projectDirectory.getId(), user.getId());
+      entityManager.flush();
+    });
   }
 }
